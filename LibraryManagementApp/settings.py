1352ee033382c1b4e2db77daa7847b7c6a7e58ea
smitPatel1302 <smit.dpatel9924@gmail.com>
2022-11-06 10:20:51 +0530

    make django app ready to host on heroku

--- LibraryManagementApp/settings.py
+++ LibraryManagementApp/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c^#@4w$xx9hl2pxjue*_to#j*$7=c%$qjbe1(h0dng@7@)44=('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'bookappwithstore.herokuapp.com']
 
 
 # Application definition
@@ -26,10 +26,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'index',
+    'store',
+    'about',
+    'auth',
+    'contactus'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -61,13 +67,6 @@ WSGI_APPLICATION = 'LibraryManagementApp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -108,6 +107,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR , "static")
 ]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT find_my_book.wsgi:application
