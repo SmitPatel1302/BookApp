1fc9af00b136d21795ba1ff65f14be2e5f1ce0c0
smitPatel1302 <smit.dpatel9924@gmail.com>
2022-11-21 13:56:54 +0530

    change in the vercel configure file

--- LibraryManagementApp/settings.py
+++ LibraryManagementApp/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-c^#@4w$xx9hl2pxjue*_to#j*$7=c%$qjbe1(h0dng@7@)44=(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'bookappwithstore.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'bookappwithstore.herokuapp.com','*']
 
 
 # Application definition
